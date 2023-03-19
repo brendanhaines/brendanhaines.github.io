b4afe2af76217ea79cda6df363d954023e4a5974
Brendan Haines <brendan.haines@gmail.com>
2023-03-18 23:24:08 -0600

    replace icons

--- assets/image/generate.py
+++ assets/image/generate.py
@@ -4,13 +4,17 @@ from pathlib import Path
 image_ext = ["jpg", "png"]
 
 dir_avatar = Path(__file__).parent
-orig = [f for f in dir_avatar.iterdir() if f.stem == "avatar_orig" and f.suffix[1:] in image_ext]
-if len(orig) == 0:
-    raise FileNotFoundError(f'Could not find file named "avatar_orig.[{"|".join(image_ext)}]"')
-elif len(orig) > 1:
-    raise ValueError(f"Ambiguous source image. Found {', '.join([p.name for p in orig])}")
+# orig = [f for f in dir_avatar.iterdir() if f.stem == "avatar_orig" and f.suffix[1:] in image_ext]
+# if len(orig) == 0:
+#     raise FileNotFoundError(f'Could not find file named "avatar_orig.[{"|".join(image_ext)}]"')
+# elif len(orig) > 1:
+#     raise ValueError(f"Ambiguous source image. Found {', '.join([p.name for p in orig])}")
+# else:
+#     orig = orig[0]
+orig = dir_avatar / "logo_64x64.png"
+# orig = dir_avatar / "logo.png"
 
-with Image.open(orig[0]) as im:
+with Image.open(orig) as im:
     if im.size[0] != im.size[1]:
         s = min(im.size)
         left = (im.size[0] - s)/2
@@ -24,3 +28,6 @@ with Image.open(orig[0]) as im:
 
     for s in [16, 32, 57, 60, 70, 72, 76, 96, 114, 120, 128, 144, 150, 152, 180, 192, 310, 384, 512]:
         im.resize([min(im.size[0], s)] * 2).save(dir_avatar / f"favicon-{s}x{s}.png")
+    
+    im.resize([min(im.size[0], 70)] * 2).save(dir_avatar / f"favicon.ico")
+    
